217cde558bfa0e27026a3007bfb3898712af7f24
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2023-05-15 19:27:26 +0900

    명철호
    1. 박스 프리팹화
    2. 박스 Layer, tag 추가
    7. Tag에 Palyer추가
    
    작업시
    3. 박스 Rigid 중력 0으로 작업
    4. 박스 Rigid x,y,z 다 잠굼으로 작업
    5. 박스 transform 소수점 3자리 떨어지게 않게 하기
    6. 박스 collider size는 0.9로
    1로하면 낑겨서 안움직임.

---
+++ Assets/Script/Player/Player_Action.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 방향 상수 값
+static class Constants
+{
+    public const short DR = 1;
+    public const short DL = 2;
+    public const short DU = 3;
+    public const short DD = 4;
+
+}
+
+public class Player_Action : MonoBehaviour
+{
+    public float speed;
+
+
+    // 캐릭터 방향
+    short direction;
+    Vector3 dirVec;
+    bool isCharacterMove;
+    float isCharacterTime;
+
+    // h : horizontal , v : vertical
+    float h;
+    float v;
+
+    bool isHorizonMove;
+
+    /* 값 가져오기 */
+    public Rigidbody2D rigid;
+
+    /*애니메이션 */
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    private void Start()
+    {
+        direction = Constants.DD;
+        dirVec = Vector3.down;
+        isCharacterTime = 0f;
+        isCharacterMove = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //키보드 입력 받는 메소드
+        Player_Move();
+        //실제 게임 velocity 주는 메소드
+        Player_velocity();
+    }
+
+    void FixedUpdate()
+    {
+        if (isCharacterMove)
+        {
+            isCharacterTime += Time.deltaTime;
+            if (isCharacterTime >= 0.7f)
+            {
+                isCharacterMove = false;
+                isCharacterTime = 0f;
+            }
+        }
+    }
+
+    void Player_Move()
+    {
+        if (isCharacterMove)
+        {
+            rigid.constraints = RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
+
+            anim.SetInteger("hRaw", 0);
+            anim.SetInteger("vRaw", 0);
+
+        }
+        else
+        {
+            rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+            /* <- 방향 Value -1 , -> 방향 Value 1 */
+            h = Input.GetAxisRaw("Horizontal");
+            /* 아래 방향 Value -1, 윗 방향 Value 1 */
+            v = Input.GetAxisRaw("Vertical");
+
+            bool hDown = Input.GetButtonDown("Horizontal");
+            bool hUp = Input.GetButtonUp("Horizontal");
+            bool vDown = Input.GetButtonDown("Vertical");
+            bool vUp = Input.GetButtonUp("Vertical");
+
+            // 방향전환 시점
+            if (hDown)
+            {
+                isHorizonMove = true;
+
+            }
+            else if (vDown)
+            {
+                isHorizonMove = false;
+
+            }
+            else if (hUp || vUp)
+            {
+                isHorizonMove = h != 0;
+            }
+
+            // 애니메이션 [ h가 가로 !! v는 세로 !! ]
+            if (anim.GetInteger("hRaw") != h)
+            {
+                anim.SetBool("isMoveDirection", true);
+                anim.SetInteger("hRaw", (int)h);
+            }
+            else if (anim.GetInteger("vRaw") != v)
+            {
+                anim.SetBool("isMoveDirection", true);
+                anim.SetInteger("vRaw", (int)v);
+            }
+            else
+            {
+                anim.SetBool("isMoveDirection", false);
+            }
+
+
+        }
+    }
+
+    void Player_velocity()
+    {
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        if (moveVec == Vector2.right)
+        {
+            direction = Constants.DR;
+            dirVec = Vector3.right;
+        }
+        else if (moveVec == Vector2.left)
+        {
+            direction = Constants.DL;
+            dirVec = Vector3.left;
+        }
+        else if (moveVec == Vector2.down)
+        {
+            direction = Constants.DD;
+            dirVec = Vector3.down;
+        }
+        else if (moveVec == Vector2.up)
+        {
+            direction = Constants.DU;
+            dirVec = Vector3.up;
+        }
+
+        rigid.velocity = moveVec * speed;
+    }
+
+    //캐릭터 방향 getter
+    public short get_s_dir()
+    {
+        return direction;
+    }
+
+    public Vector3 get_v_dir()
+    {
+        return dirVec;
+    }
+
+    public void isCharacterSetter(bool isCharacter)
+    {
+        isCharacterMove = isCharacter;
+    }
+
+    // 충돌
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Box")
+        {
+            Debug.Log("박스와 충돌함 !! ");
+
+        }
+    }
+
+}
+
---
+++ Assets/Script/Player/player_Raycast.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_Raycast : MonoBehaviour
+{
+
+    Rigidbody2D rigid;
+    Player_Action _pA;
+    GameObject scanObject;
+    bool canTransTo;
+    float canTransTime;
+
+    void Start()
+    {
+        canTransTo = false;
+        canTransTime = 0f;
+        rigid = GetComponent<Rigidbody2D>();
+        _pA = GetComponent<Player_Action>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(rigid.position, _pA.get_v_dir() * 0.5f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, _pA.get_v_dir(), 0.5f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider.tag == "Box" && !canTransTo)
+        {
+            canTransTo = true;
+            scanObject = rayHit.collider.gameObject;
+            Debug.Log("스캔된 오브젝트 이름 : " + scanObject.name + "입니다.");
+            //박스 움직이는 메소드
+            scanObject.GetComponent<moveBox>().setisReady(true);
+
+            //캐릭터 움직임 잠구는 메소드
+            _pA.isCharacterSetter(true);
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (canTransTo)
+        {
+            canTransTime += Time.deltaTime;
+            if (canTransTime >= 1f)
+            {
+                canTransTime = 0;
+                canTransTo = false;
+            }
+        }
+
+
+
+    }
+
+
+
+
+}
