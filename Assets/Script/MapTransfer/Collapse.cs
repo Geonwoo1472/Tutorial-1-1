61c068cf2c71cef9bd0d4cf86f66094fc4f941d1
Myoungcholho <mch473700@gmail.com>
2023-08-14 17:08:11 +0900

    명철호
    동굴 기능 업로드/
    동굴 배치작업 끝

--- Assets/Script/MapTransfer/Collapse.cs
+++ Assets/Script/MapTransfer/Collapse.cs
@@ -1,28 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Collapse : MonoBehaviour
-{    
+{
     public GameObject connectedCave;        // 반대편 동굴 정보
     public Sprite activeImage;			    // 변경될 이미지
+    public Transform TeleportPos;			// 이동할 위치
 
     [HideInInspector]
     public bool isActive;                   // 동굴을 사용했는지
 
-    private Transform playerPos;            // 플레이어 위치
+    private Transform playerPos;            // 플레이어 위치 
     private Collapse oppositeCollapse;      // 반대편 동굴의 스크립트
-    private Transform TeleportPos;			// 이동할 위치
     private SpriteRenderer spriteRender;    // Sprite컴포넌트
-    
+    private BoxCollider2D boxCollider;      // boxCollider 컴포넌트 
+
     void Start()
     {
         isActive = false;
 
         playerPos = GameObject.Find("Player").GetComponent<Transform>();
         oppositeCollapse = connectedCave.GetComponent<Collapse>();
-        TeleportPos = connectedCave.transform;
         spriteRender = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
     /*
      Triger가 체크되어 있는 경우 발동.
@@ -33,13 +33,26 @@ public class Collapse : MonoBehaviour
      */
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        playerPos = TeleportPos;
-        setActive(true);
-        oppositeCollapse.setActive(true);
+        if (isActive)
+            return;
+
+        if (collision.CompareTag("Player"))
+        {
+            collapse();
+        }
     }
+
     public void setActive(bool _isActive)
     {
         isActive = _isActive;
         spriteRender.sprite = activeImage;
+        boxCollider.isTrigger = false;
+    }
+
+    private void collapse()
+    {
+        playerPos.position = TeleportPos.position;
+        setActive(true);
+        oppositeCollapse.setActive(true);
     }
 }
