d4b057f11d79270174e123c23c5f0c888d221b9b
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2023-08-18 21:25:26 +0900

    명철호
    스크립트 정리/

--- Assets/Script/Talk/AutoTalk.cs
+++ Assets/Script/Talk/AutoTalk.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class AutoTalk : MonoBehaviour
 {
-    private DalogManager manager;
-    private ObjectTalkData objectTalkData;
+    private DalogManager manager;                   // DalogManger.cs
+    private ObjectTalkData objectTalkData;          // ObjectTalkData.cs
     private void Start()
     {
         manager = GameObject.Find("DalogManager").GetComponent<DalogManager>();
         objectTalkData = GetComponent<ObjectTalkData>();
     }
 
+
+    /* 
+    만약 플레이어가 트리거 작동을 했다면
+    다시 활성화 되지 않도록 내부 bool 변수를 변경하고
+    Action (대화 시작 메소드) 를 시작합니다.
+     */
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
--- Assets/Script/Talk/DalogManager.cs
+++ Assets/Script/Talk/DalogManager.cs
@@ -5,19 +5,23 @@ using UnityEngine.UI;
 
 public class DalogManager : MonoBehaviour
 {
-    public TypeEffect talk;
-    public GameObject talkPanel;
-    public bool isAction;
-    public TalkManager talkManager;
-    public Image portraitImg;
-    public int talkIndex;
+    public TypeEffect talk;                     //TypeEffect.cs
+    public GameObject talkPanel;                //인게임 말풍선 판넬 이미지
+    public bool isAction;                       // 
+    public TalkManager talkManager;             //talkManger.cs
+    public Image portraitImg;                   //NPC 이미지
+    public int talkIndex;                       //현재 문자열 인덱스 구분
 
 
     private GameObject scanObject;
     private ObjectTalkData objData;
     
 
-
+    /* 
+     스캔된 오브젝트의 ID를 확인하고
+     Talk 메소드를 호출합니다.
+    이후 말풍선 판넬의 활성유무를 결정합니다.
+     */
     public void Action(GameObject scanObj)
     {
         scanObject = scanObj;
@@ -27,6 +31,9 @@ public class DalogManager : MonoBehaviour
         talkPanel.SetActive(isAction);
     }
 
+    /* 
+     
+     */
     void Talk(int ID, bool isNpc)
     {
         string talkData = "";
