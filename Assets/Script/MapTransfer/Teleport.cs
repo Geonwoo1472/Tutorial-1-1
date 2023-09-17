4e2e393d25910b5b4aa2924b10730b34f4019948
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2023-09-17 14:15:12 +0900

    명철호
    코드 필요없는 부분 버림

--- Assets/Script/MapTransfer/Teleport.cs
+++ Assets/Script/MapTransfer/Teleport.cs
@@ -3,26 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Teleport : MonoBehaviour
 {
-    public GameObject connectedCave;        // 반대편 동굴 정보
-    public Sprite activeImage;			    // 변경될 이미지
     public Transform teleportPos;			// 이동할 위치
-
-    [HideInInspector]
-    public bool isActive;                   // 동굴을 사용했는지
-
     private Transform playerPos;            // 플레이어 위치 
-    private Collapse oppositeCollapse;      // 반대편 동굴의 스크립트
-    private SpriteRenderer spriteRender;    // Sprite컴포넌트
-    private BoxCollider2D boxCollider;      // boxCollider 컴포넌트 
 
     void Start()
     {
-        //isActive = false;
-
         playerPos = GameObject.Find("Player").GetComponent<Transform>();
-        oppositeCollapse = connectedCave.GetComponent<Collapse>();
-        spriteRender = GetComponent<SpriteRenderer>();
-        //boxCollider = GetComponent<BoxCollider2D>();
     }
     /*
      Triger가 체크되어 있는 경우 발동.
@@ -33,27 +19,13 @@ public class Teleport : MonoBehaviour
      */
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        /*
-         if (isActive)
-            return;
-        */
         if (collision.CompareTag("Player"))
         {
             Collapsing();
         }
     }
-
-    public void SetActive(bool _isActive)
-    {
-        isActive = _isActive;
-        spriteRender.sprite = activeImage;
-        boxCollider.isTrigger = false;
-    }
-
     protected virtual void Collapsing()
     {
         playerPos.position = teleportPos.position;
-        SetActive(true);
-        oppositeCollapse.SetActive(true);
     }
 }
