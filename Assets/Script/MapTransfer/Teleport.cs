1e0e2334d9a632dfbba37d5731b5dc8b1951d013
YunHyeunSeung <yunheunseung0816@gmail.com>
2023-09-18 16:58:17 +0900

    윤현승
    랜덤 텔레포트 진행중(중복만 해결하면 끝)

--- Assets/Script/MapTransfer/Teleport.cs
+++ Assets/Script/MapTransfer/Teleport.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Teleport : MonoBehaviour
 {
-    public Transform teleportPos;			// 이동할 위치
+    public int maxNumber;                 // 난수 발생할 포트 넘버
+    private GameObject connectedCave;       // 반대현 동굴 정보
+    private Transform teleportPos;			// 이동할 위치
     private Transform playerPos;            // 플레이어 위치 
 
     void Start()
     {
+        int num = RandomNumber(maxNumber);
+        Debug.Log(num);
+        connectedCave = GameObject.Find("Teleport " + num);
+        teleportPos = GameObject.Find("Spwan " + num).GetComponent<Transform>();
         playerPos = GameObject.Find("Player").GetComponent<Transform>();
     }
-    /*
-     Triger가 체크되어 있는 경우 발동.
-    플레이어의 위치를 지정한 위치로 이동시키고
-    다시 사용하지 못하도록 bool 값을 변경하고,
-    동굴 이미지를 바꿉니다.
-    이후 반대편 동굴도 같은 작업을 반복합니다.
-     */
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -27,5 +27,21 @@ public class Teleport : MonoBehaviour
     protected virtual void Collapsing()
     {
         playerPos.position = teleportPos.position;
+    }
+
+    public int RandomNumber(int a)
+    {
+        int b = Random.Range(1, a + 1);
+        int[] number = new int[a];
+
+        if(number[b - 1] == 0)
+        {
+            number[b - 1] = b;
+            return number[b - 1];
+        }
+        else
+        {
+            return RandomNumber(a);
+        }
     }
 }
